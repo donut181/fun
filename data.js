cf848d6dd342a7f6bb8d60abea41fbbb6bbcee91
mpolok <mpro12@gmail.com>
2018-03-07 00:14:41 +0100

    html5 declaration + function returning keycodes

--- data.js
+++ data.js
@@ -1,22 +1,51 @@
-var player1 = (function(){
-	var player = {
-		_x:10,
-		_y:10,
-		_spdX : 2,
-		_spdY : 2,
-		_name : "",
-		_keyBindings: ['w','s','a','d','f','g','h'],
-		_pressingRight: false,
-		_pressingLeft : false,
-		_pressingDown : false,
-		_pressingUp   : false,
-	}
-	return {
-		name: _name,
-		
-	};
-})();
+var player1 = {
+	x:10,
+	y:10,
+	spdX : 2,
+	spdY : 2,
+	name : "",
+	keyBindings: ['w','s','a','d','f','g','h'],
+	pressingRight: false,
+	pressingLeft : false,
+	pressingDown : false,
+	pressingUp   : false,
+}
 
-function letterToCode(letter){
-	return letter.charCodeAt(0);
+function KeyboardKeyCode(letter){
+	switch(letter){
+		case 'a': return 65;
+		case 'b': return 66;
+		case 'c': return 67;
+		case 'd': return 68;
+		case 'e': return 66;
+		case 'f': return 67;
+		case 'g': return 68;
+		case 'h': return 66;
+		case 'i': return 67;
+		case 'j': return 68;
+		case 'k': return 66;
+		case 'l': return 67;
+		case 'm': return 68;
+		case 'n': return 66;
+		case 'o': return 67;
+		case 'p': return 68;
+		case 'q': return 66;
+		case 'r': return 67;
+		case 's': return 68;
+		case 't': return 66;
+		case 'u': return 67;
+		case 'v': return 68;
+		case 'w': return 66;
+		case 'x': return 67;
+		case 'y': return 68;
+		case 'z': return 66;
+		case 'c': return 67;
+		case 'd': return 68;
+		case 'b': return 66;
+		case 'c': return 67;
+		case 'd': return 68;
+		case 'b': return 66;
+		case 'c': return 67;
+		case 'd': return 68;
+	}
 }
